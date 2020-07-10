1accb5842cb8b9f008dfc188380af3e23e10bc39
Esteva <ee699@bigpurple-ln1.cm.cluster>
2020-07-10 13:30:04 -0400

    changing from 4 gpus to 1

--- module_2.s
+++ module_2.s
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=gpu4_dev
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --tasks-per-node=1
 #SBATCH --job-name=analyize_classification_0
 #SBATCH --output=analyze_classifcation.o%j
